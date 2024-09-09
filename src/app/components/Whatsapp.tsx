9b6b0c7cf1738af1211421c575013e1cfcc7a1ba
Wilkinson <wilkinsondjm@gmail.com>
2024-09-09 16:01:17 -0300

    first commit

--- src/app/components/Whatsapp.tsx
+++ src/app/components/Whatsapp.tsx
@@ -6,7 +6,7 @@ return (
 
 <div className="fixed right-5 bottom-8 md:group">
     <a href="https://wa.me/" target='_blank' rel="noreferrer"
-        className="flex items-center justify-center text-white bg-green-600 rounded-full w-14 h-14 hover:bg-green-700 focus:ring-2 focus:outline-none">
+        className="flex items-center justify-center text-white bg-green-600 rounded-full w-14 h-14 hover:bg-green-700">
         <MdWhatsapp size={35} />
     </a>
 </div>
